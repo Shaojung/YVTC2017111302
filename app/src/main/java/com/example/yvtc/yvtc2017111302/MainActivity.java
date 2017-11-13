047a967d8d3443c69c336cb16944ec0e5435c8cc
Shaojung <shaojung@gmail.com>
2017-11-13 16:05:55 +0800

    RecyclerView

--- app/src/main/java/com/example/yvtc/yvtc2017111302/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc2017111302/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.yvtc.yvtc2017111302;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.yvtc.yvtc2017111302.data.Student;
 import com.example.yvtc.yvtc2017111302.data.StudentDAOMemoryImpl;
@@ -26,8 +29,6 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
         t.add(new Student("Bob", "123", "123"));
         t.add(new Student("Mary", "123", "123"));
-        mAdapter = new MyAdapter(MainActivity.this, t.getData());
-        mRecyclerView.setAdapter(mAdapter);
 
 
 
@@ -49,5 +50,25 @@ public class MainActivity extends AppCompatActivity {
 //        {
 //            Log.d("DATAS", "update:" + s.toString());
 //        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapter = new MyAdapter(MainActivity.this, t.getData());
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("ADD");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent it = new Intent(MainActivity.this, AddActivity.class);
+        startActivity(it);
+        return super.onOptionsItemSelected(item);
     }
 }
