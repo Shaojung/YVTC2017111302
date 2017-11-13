0858d4436cdd5a9f15120cb9920c6c633215758f
Shaojung <shaojung@gmail.com>
2017-11-13 15:25:59 +0800

    RecyclerView

--- app/src/main/java/com/example/yvtc/yvtc2017111302/MyAdapter.java
+++ app/src/main/java/com/example/yvtc/yvtc2017111302/MyAdapter.java
@@ -1,34 +1,49 @@
 package com.example.yvtc.yvtc2017111302;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.yvtc.yvtc2017111302.data.Student;
 
 /**
  * Created by yvtc on 2017/11/13.
  */
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    Student data[];
+    Context context;
+    public MyAdapter(Context context, Student[] data)
+    {
+        this.context = context;
+        this.data = data;
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_text_view, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        holder.tv.setText(data[position].name);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.length;
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder
     {
+        TextView tv;
         public ViewHolder(View itemView) {
             super(itemView);
+            tv = (TextView) itemView.findViewById(R.id.textView);
         }
     }
 }
