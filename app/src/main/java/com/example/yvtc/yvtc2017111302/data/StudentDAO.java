9ae25ad3af44bdd979b0bdecd3aff5c0d2808f81
Shaojung <shaojung@gmail.com>
2017-11-13 14:05:42 +0800

    DAO Test1

---
+++ app/src/main/java/com/example/yvtc/yvtc2017111302/data/StudentDAO.java
@@ -0,0 +1,11 @@
+package com.example.yvtc.yvtc2017111302.data;
+
+/**
+ * Created by yvtc on 2017/11/13.
+ */
+
+public interface StudentDAO {
+    void add(Student s);
+    Student[] getData();
+    void update(Student s);
+}
---
+++ app/src/main/java/com/example/yvtc/yvtc2017111302/data/StudentDAOMemoryImpl.java
@@ -0,0 +1,22 @@
+package com.example.yvtc.yvtc2017111302.data;
+
+/**
+ * Created by yvtc on 2017/11/13.
+ */
+
+public class StudentDAOMemoryImpl implements StudentDAO {
+    @Override
+    public void add(Student s) {
+
+    }
+
+    @Override
+    public Student[] getData() {
+        return new Student[0];
+    }
+
+    @Override
+    public void update(Student s) {
+
+    }
+}
