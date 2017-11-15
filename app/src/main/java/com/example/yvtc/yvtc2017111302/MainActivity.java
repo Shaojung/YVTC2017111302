db232195b210ff958ee84ebc565215fc10f85e3e
Shaojung <shaojung@gmail.com>
2017-11-15 11:50:45 +0800

    RecyclerView

--- app/src/main/java/com/example/yvtc/yvtc2017111302/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc2017111302/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -18,11 +19,12 @@ import com.example.yvtc.yvtc2017111302.data.StudentDAOTest1;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecyclerView.OnItemTouchListener {
     public static StudentDAOMemoryImpl t = new StudentDAOMemoryImpl();
     RecyclerView mRecyclerView;
     RecyclerView.Adapter<MyAdapter.ViewHolder> mAdapter;
     RecyclerView.LayoutManager mLayoutManager;
+    GestureDetector mGD;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -32,31 +34,16 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
         t.add(new Student("Bob", "123", "123"));
         t.add(new Student("Mary", "123", "123"));
-        mRecyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
-            @Override
-            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-                View v = rv.findChildViewUnder(e.getX(), e.getY());
-                int position = rv.getChildLayoutPosition(v);
-                // Toast.makeText(MainActivity.this, "posi:" + position, Toast.LENGTH_SHORT).show();
-                if (position >= 0)
-                {
-                    Intent it = new Intent(MainActivity.this, DetailActivity.class);
-                    it.putExtra("id", t.getData()[position].id);
-                    startActivity(it);
+        mGD = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener()
+            {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
                 }
-                return false;
             }
+        );
+        mRecyclerView.addOnItemTouchListener(this);
 
-            @Override
-            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-
-            }
-
-            @Override
-            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-            }
-        });
     }
 
     @Override
@@ -77,5 +64,33 @@ public class MainActivity extends AppCompatActivity {
         Intent it = new Intent(MainActivity.this, AddActivity.class);
         startActivity(it);
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        View v = rv.findChildViewUnder(e.getX(), e.getY());
+        if (mGD.onTouchEvent(e))
+        {
+            int position = rv.getChildLayoutPosition(v);
+            // Toast.makeText(MainActivity.this, "posi:" + position, Toast.LENGTH_SHORT).show();
+            if (position >= 0)
+            {
+                Intent it = new Intent(MainActivity.this, DetailActivity.class);
+                it.putExtra("id", t.getData()[position].id);
+                startActivity(it);
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
     }
 }
