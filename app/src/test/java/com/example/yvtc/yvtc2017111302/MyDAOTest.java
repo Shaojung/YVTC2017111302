5fb75975805ddccc3ddb17f3feb6491ceabf7e57
Shaojung <shaojung@gmail.com>
2017-11-15 08:40:54 +0800

    RecyclerView

---
+++ app/src/test/java/com/example/yvtc/yvtc2017111302/MyDAOTest.java
@@ -0,0 +1,17 @@
+package com.example.yvtc.yvtc2017111302;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by yvtc on 2017/11/15.
+ */
+
+public class MyDAOTest {
+    @Test
+    public void MyTest1()
+    {
+        assertEquals(3+3,6);
+    }
+}
