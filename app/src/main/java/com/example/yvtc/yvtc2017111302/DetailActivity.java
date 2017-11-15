dc710ca3940b0ff29129987f0583a4b5dde6d99e
Shaojung <shaojung@gmail.com>
2017-11-15 13:59:31 +0800

    RecyclerView

--- app/src/main/java/com/example/yvtc/yvtc2017111302/DetailActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc2017111302/DetailActivity.java
@@ -1,13 +1,17 @@
 package com.example.yvtc.yvtc2017111302;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.yvtc.yvtc2017111302.data.Student;
 
 public class DetailActivity extends AppCompatActivity {
     TextView tv2, tv3, tv4, tv5;
+    Student stu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +22,39 @@ public class DetailActivity extends AppCompatActivity {
         tv5 = (TextView) findViewById(R.id.textView5);
 
         int id = getIntent().getIntExtra("id", -1);
-        Student stu = MainActivity.t.getOneStudent(id);
+        stu = MainActivity.t.getOneStudent(id);
         tv2.setText(String.valueOf(stu.id));
         tv3.setText(stu.name);
         tv4.setText(stu.tel);
         tv5.setText(stu.addr);
+    }
+    public void clickDelete(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delele_confirm);
+        builder.setPositiveButton(R.string.okay, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.t.delete(stu);
+                finish();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
+            }
+        });
+        builder.show();
+    }
+
+    public void clickEdit(View v)
+    {
+
+    }
+
+    public void clickBack(View v)
+    {
+        finish();
     }
 }
