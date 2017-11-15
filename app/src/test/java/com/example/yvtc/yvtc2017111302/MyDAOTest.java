6084800de51a2f68007210689538634bdcd2e57c
Shaojung <shaojung@gmail.com>
2017-11-15 08:58:23 +0800

    RecyclerView

--- app/src/test/java/com/example/yvtc/yvtc2017111302/MyDAOTest.java
+++ app/src/test/java/com/example/yvtc/yvtc2017111302/MyDAOTest.java
@@ -1,5 +1,8 @@
 package com.example.yvtc.yvtc2017111302;
 
+import com.example.yvtc.yvtc2017111302.data.Student;
+import com.example.yvtc.yvtc2017111302.data.StudentDAOMemoryImpl;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -13,5 +16,26 @@ public class MyDAOTest {
     public void MyTest1()
     {
         assertEquals(3+3,6);
+    }
+
+    @Test
+    public void TestMemoryDAO1()
+    {
+        StudentDAOMemoryImpl dao = new StudentDAOMemoryImpl();
+        dao.add(new Student("AA", "11", "aabb"));
+        dao.add(new Student("BB", "22", "aabb"));
+        dao.add(new Student("CC", "33", "aabb"));
+        Student[] stus = dao.getData();
+        assertEquals(stus.length, 3);
+    }
+    @Test
+    public void TestMemoryDAO2()
+    {
+        StudentDAOMemoryImpl dao = new StudentDAOMemoryImpl();
+        dao.add(new Student("AA", "11", "aabb"));
+        dao.add(new Student("BB", "22", "aabb"));
+        dao.add(new Student("CC", "33", "aabb"));
+        Student[] stus = dao.getData();
+        assertEquals(stus[1].tel, "22");
     }
 }
