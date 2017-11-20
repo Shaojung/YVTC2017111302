8a99fb223b5b240cd3595064f3b7e09ef66333ba
Shaojung <shaojung@gmail.com>
2017-11-20 13:57:52 +0800

    Student File Implement

---
+++ app/src/main/java/com/example/yvtc/yvtc2017111302/data/StudentDAODBImpl.java
@@ -0,0 +1,42 @@
+package com.example.yvtc.yvtc2017111302.data;
+
+/**
+ * Created by yvtc on 2017/11/20.
+ */
+
+public class StudentDAODBImpl implements StudentDAO {
+    @Override
+    public void add(Student s) {
+
+    }
+
+    @Override
+    public Student[] getData() {
+        return new Student[0];
+    }
+
+    @Override
+    public void update(Student s) {
+
+    }
+
+    @Override
+    public void delete(Student s) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Student getOneStudent(int id) {
+        return null;
+    }
+
+    @Override
+    public Student[] searchByName(String name) {
+        return new Student[0];
+    }
+}
