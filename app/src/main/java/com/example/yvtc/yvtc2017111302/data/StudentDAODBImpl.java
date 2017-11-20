f13f352e6b1981ffbd944f342975868d74c48292
Shaojung <shaojung@gmail.com>
2017-11-20 14:05:55 +0800

    Ready to use SQLite

--- app/src/main/java/com/example/yvtc/yvtc2017111302/data/StudentDAODBImpl.java
+++ app/src/main/java/com/example/yvtc/yvtc2017111302/data/StudentDAODBImpl.java
@@ -1,10 +1,18 @@
 package com.example.yvtc.yvtc2017111302.data;
 
+import android.content.Context;
+
 /**
  * Created by yvtc on 2017/11/20.
  */
 
 public class StudentDAODBImpl implements StudentDAO {
+    Context context;
+    public StudentDAODBImpl(Context context)
+    {
+        this.context = context;
+    }
+
     @Override
     public void add(Student s) {
 
